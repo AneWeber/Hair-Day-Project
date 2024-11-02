29c9e11dd29dcb95be83fdae37943110f0e51da1
Ane Weber <anemariweber@gmail.com>
2024-11-02 17:44:44 +0100

    code developing

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+//css
+import "../styles/global.css"
+import "../styles/form.css"
+import "../styles/schedule.css"
+//day.js
+import "../libs/dayjs.js"
+
+
+/* testando se o day ta funcionando:
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm"))
+*/
